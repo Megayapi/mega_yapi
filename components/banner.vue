04b8006689a3fd29b37369752f1ec54bee490441
Atlas <atlasyigitaydin@gmail.com>
2024-10-20 18:14:17 +0300

    banner edit

--- components/banner.vue
+++ components/banner.vue
@@ -8,10 +8,13 @@ const banner = ref<boolean>(false)
       :class="`flex gap-2 h-12 text-gray-950 rounded-r-xl ${banner ? 'bg-green-400' : 'bg-primary-400'} cursor-pointer flex justify-center items-center p-1`"
       @click="banner = !banner"
     >
-      <div class="text-sm font-light text-center">
+      <div
+
+        class="text-sm font-light text-center"
+      >
         İnstagramdan Bizi Takip Ederek %5 İndirim Kazanın!
       </div>
-      <Icon class="text-3xl" :name="banner ? 'hugeicons:discount' : 'uiw:left'" />
+      <Icon class="text-3xl w-[30px]" :name="banner ? 'hugeicons:discount' : 'uiw:left'" />
     </div>
   </div>
 </template>
